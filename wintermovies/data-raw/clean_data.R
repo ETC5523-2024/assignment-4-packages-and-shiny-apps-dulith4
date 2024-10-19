a5b8638b0ab9e711aafda2b127b0bd06a8ab4bbf
Dulitha Perera <mdul0002@student.monash.edu>
2024-10-19 19:27:19 +1100

    cleaning the datasets

---
+++ wintermovies/data-raw/clean_data.R
@@ -0,0 +1,18 @@
+library(readr)
+library(dplyr)
+library(tidyr)
+
+# Read the data
+holiday_movies <- read_csv("data-raw/holiday_movies.csv")
+holiday_movie_genres <- read_csv("data-raw/holiday_movie_genres.csv")
+
+# Remove irrelevant genres
+irrelevant_genres <- c("Music", "Talk-Show", "News", "Reality-TV", "Sport")
+cleaned_holiday_movie_genres <- holiday_movie_genres %>%
+  filter(!genres %in% irrelevant_genres)
+
+# Convert holiday columns to integer for analysis
+holiday_movies <- holiday_movies %>%
+  mutate(across(c(christmas, hanukkah, kwanzaa), as.integer))
+
+
