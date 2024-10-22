d76adf9d75a30a78d0200ffdf017d5d1a6bafc99
Dulitha Perera <mdul0002@student.monash.edu>
2024-10-22 13:11:42 +1100

    changes in shiny app

--- wintermovies/inst/app.R
+++ wintermovies/inst/app.R
@@ -9,7 +9,7 @@ library(bslib)
 load(system.file("data", "holiday_movies.rda", package = "wintermovies"))
 load(system.file("data", "cleaned_holiday_movie_genres.rda", package = "wintermovies"))
 
-# Define UI for the Shiny app
+
 
 ui <- fluidPage(
   theme = bs_theme(bootswatch = "minty"),
@@ -37,10 +37,10 @@ ui <- fluidPage(
     mainPanel(
       tableOutput("moviesTable"),
 
-      # Provide interpretation for the outputs
+
       p("Interpretation: This table helps you explore the holiday movies dataset based on your selected year and genre."),
 
-      # Static interactive Plotly bar plot for overall genre distribution
+
       div(class = "user-select-none_svg-container",
           plotlyOutput("genreBarPlot")
       ),
@@ -51,17 +51,17 @@ ui <- fluidPage(
   )
 )
 
-# Define server logic for filtering data and rendering the table and static plot
+
 server <- function(input, output) {
 
-  # Reactive data based on selected year and genre
+
   filtered_movies <- reactive({
     holiday_movies %>%
       filter(year == input$year) %>%
       semi_join(cleaned_holiday_movie_genres %>% filter(genres == input$genre), by = "tconst")
   })
 
-  # Table output showing the filtered movies
+
   output$moviesTable <- renderTable({
     filtered_movies() %>%
       mutate(
@@ -71,18 +71,18 @@ server <- function(input, output) {
       )
   })
 
-  # Static Plotly bar plot showing the count of all genres across the entire dataset
+
   output$genreBarPlot <- renderPlotly({
-    # Count the occurrences of each genre across the whole dataset
+
     genre_counts <- cleaned_holiday_movie_genres %>%
       count(genres) %>%
-      arrange(desc(n))  # Arrange in descending order for better visualization
+      arrange(desc(n))
 
-    # Convert genres to factor to preserve the descending order
+
     genre_counts$genres <- factor(genre_counts$genres, levels = genre_counts$genres)
 
     # Create a color vector: red for top 5 genres, blue for the rest
-    colors <- ifelse(1:nrow(genre_counts) <= 5, 'red', 'steelblue')
+    colors <- ifelse(1:nrow(genre_counts) <= 5, 'red', 'darkgreen')
 
     # Create an interactive Plotly bar plot with custom colors
     plot_ly(genre_counts, x = ~genres, y = ~n, type = 'bar', marker = list(color = colors)) %>%
